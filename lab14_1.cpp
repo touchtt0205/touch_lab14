b6d9da578b491b9437a5a7a6ad164af87725e8f0
sippakon khammisawang <sippakon_k@cmu.ac.th>
2023-01-17 02:41:12 +0700

    full

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 void stat(const double[],int,double[]);
-
 int main(){
     double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
     int N = sizeof(A)/sizeof(A[0]);
@@ -16,4 +14,48 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double a[] , int n , double b[]){
+    int i;
+    double sum;
+    for(i=0;i<n;i++){
+        sum+=a[i];
+    }
+    sum = sum/n;
+    b[0]=sum;
+
+    float sd;
+    double u;
+    for(i=0;i<n;i++){
+        u += pow(a[i],2);
+    }
+        double e = u/n;
+        sd = sqrt(e-(pow(sum,2)));
+        b[1]=sd;
+
+    float gm;
+    float g=1;
+    for(i=0;i<n;i++){
+        g *= a[i];
+    }
+        gm = pow(g,(float)1/n);
+        b[2]=gm;
+
+    float hm;
+    float k=0;
+    for(i=0;i<n;i++){
+        k += 1/a[i];
+    }
+        hm = n/k;
+        b[3]=hm;
+
+
+    double max = a[0] , min = a[0];
+    for(i=1;i<n;i++){
+        if(a[i]>max) max=a[i];
+    b[4]=max;
+        if(a[i]<min) min=a[i];
+    b[5]=min;
+    }
 }
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,24 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(sqrt(pow(x-i,2)+pow(y-j,2))<=s-1) image[i][j]=1;
+        }
+    }
+}
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i=0;i<N;i++){
+    cout << "|";
+        for(int j=0;j<M;j++){
+            if(image[i][j]==0) cout << " ";
+            else cout << "*";
+        }
+    cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
